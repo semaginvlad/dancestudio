951c833ced7a1f63b2ccfd3c1e40725e67e22cfa
semaginvlad <semagin.vlad@gmail.com>
2026-04-17 15:54:31 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ import {
   getSubStatus,
   today,
   toLocalISO,
+  uid,
   useStickyState,
 } from "./shared/utils";
 import { Badge, Field, GroupSelect, Modal, Pill, StudentSelectWithSearch } from "./components/UI";
